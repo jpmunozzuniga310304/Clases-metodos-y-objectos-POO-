1d4e4fcd664c37fcdb99f5c2c76c47496db29b01
josep <josepablo.mz04@gmail.com>
2023-10-06 19:45:22 -0600

    Agregar archivos de proyecto.

---
+++ Clases, metodos y objectos (POO)/Persona.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace Clases__metodos_y_objectos__POO_
+{
+    public class Persona
+    {
+        private string nombre;
+        private int edad;
+        private string fechaNacimiento;
+
+        public string Nombre { get => nombre; set => nombre = value; }
+        public int Edad { get => edad; set => edad = value; }
+        public string FechaNacimiento { get => fechaNacimiento; set => fechaNacimiento = value; }
+
+        public void crearPersona(string _nombre, int _edad, string _fechaNacimiento)
+        {
+            nombre = _nombre;
+            this.edad = _edad;
+            fechaNacimiento = _fechaNacimiento;
+        }
+    }
+}
---
+++ Clases, metodos y objectos (POO)/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clases__metodos_y_objectos__POO_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Persona objPersona = new Persona();
+            objPersona.crearPersona("Marko Robles", 20, "01-01-2000");
+
+            Console.WriteLine("Nombre: " + objPersona.Nombre + " Edad: " + objPersona.Edad + " Fecha Nacimiento: " + objPersona.FechaNacimiento );
+            Console.ReadLine();
+
+        }
+    }
+}
